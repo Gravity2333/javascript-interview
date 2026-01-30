f3c0707a5c793c153e174d2912787241709c874f
liuze <liuze@machloop.com>
2026-01-30 09:29:23 +0800

    f

--- 5.Promise/消除异步传染.js
+++ 5.Promise/消除异步传染.js
@@ -2,19 +2,34 @@ function runAsync(asyncFn) {
   const context = {
     caches: [],
     pos: 0,
+    AsyncException: new Error("runAsync 内部ERR 请抛出"),
+    weakable: null,
     fetch: (url) => {
       if (context.caches[context.pos]) {
+        if(context.caches[context.pos] instanceof Error){
+          throw context.caches[context.pos]
+        }
         return context.caches[context.pos++];
       } else {
-        const weakable = new Promise((resolve) => {
-          setTimeout(() => {
-            resolve("DATA FROM MOCK FETCH" + context.pos);
-          }, 1000);
+        context.weakable = new Promise((resolve, reject) => {
+          if (context.pos === 2) {
+            reject("net err");
+          } else {
+            setTimeout(() => {
+              resolve("DATA FROM MOCK FETCH" + context.pos);
+            }, 1000);
+          }
         });
-        weakable.then((result) => {
-          context.caches[context.pos] = result;
-        });
-        throw weakable;
+        context.weakable;
+        context.weakable.then(
+          (result) => {
+            context.caches[context.pos] = result;
+          },
+          (error) => {
+            context.caches[context.pos] = new Error(error);
+          },
+        );
+        throw context.AsyncException;
       }
     },
   };
@@ -24,8 +39,12 @@ function runAsync(asyncFn) {
       context.pos = 0;
       asyncFn(context);
     } catch (err) {
-      if (err instanceof Promise) {
-        err.then(launch);
+      if (err === context.AsyncException) {
+        const weakable = context.weakable;
+        context.weakable = null;
+        weakable.then(launch,launch);
+      } else {
+        throw err;
       }
     }
   }
@@ -33,11 +52,16 @@ function runAsync(asyncFn) {
   launch();
 }
 
-function asyncFn({ fetch }) {
-  const a = fetch();
-  const b = fetch();
-  const c = fetch();
-  console.log(a,b,c);
+function asyncFn({ fetch, AsyncException }) {
+  try {
+    const a = fetch();
+    const b = fetch();
+    const c = fetch();
+    console.log(a, b, c);
+  } catch (err) {
+    if (err === AsyncException) throw AsyncException;
+    console.error("err", err);
+  }
 }
 
 runAsync(asyncFn);
